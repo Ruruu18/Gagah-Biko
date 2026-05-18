a5ff6038647a90fc11131fa71cdf9016aa746602
ruru1918 <enariomharjade@gmail.com>
2026-05-18 17:33:19 +0800

    Serve static files through Vercel function

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,10 @@
 URL configuration for config project.
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.urls import path, re_path
 from django.views import defaults
+from django.views.static import serve
 from orders import views
 
 urlpatterns = [
@@ -52,3 +54,8 @@ urlpatterns = [
 ]
 
 handler404 = 'orders.views.page_not_found'
+
+if settings.IS_VERCEL:
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.BASE_DIR / 'static'}),
+    ]
